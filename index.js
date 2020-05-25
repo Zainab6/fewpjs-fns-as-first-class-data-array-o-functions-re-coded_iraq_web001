9cae6e50fcb3cd95f08f7604fdf329b43c4fa04d
Zainab Saud <zai99nb@gmail.com>
2020-05-25 21:47:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ let array=[];
 function exerciseDog(dogName,dogBreed) {
   let i;
   for(i=0; i<routine.length; i++){
-  array = routine[i] `${dogName} ${dogBreed}`;
+  array = routine[i] (dogName,dogBreed);
+  array.push();
   }
   return array;
 }
